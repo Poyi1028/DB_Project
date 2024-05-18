61da7ab1aacfe1295091444f083058a16520470a
Poyi1028 <111306057@g.nccu.edu.tw>
2024-05-18 23:03:43 +0800

    First Commit

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, redirect, request, render_template, flash
+import pymysql
+
+app = Flask(__name__)
+app.secret_key = 'my secret key'
+
+# MySQL 連接
+def get_db_conn():
+    conn = pymysql.connect(host='localhost', user='root', password='password', database='mydb')
+    return conn
+
+# 首頁
+@app.route('/')
+def index():
+    return render_template('homepage.html')
+
+# 獲取登入視窗的資料
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+
+        # 獲取前端輸入的數據
+        id = request.form['login_id']
+        password = request.form['login_password']
+
+        # 從資料庫比對資料，有資料則登入成功
+        try:
+            conn = get_db_conn()
+            with conn.cursor() as cursor:
+                # 測試 ID 和密碼是否都正確
+                sql = 'SELECT * FROM user WHERE User_ID = %s AND Password = %s'
+                cursor.execute(sql, (id, password))
+                user = cursor.fetchone()
+                conn.commit()
+
+                if user is None:
+                    return render_template('fail.html')
+                else:
+                    return render_template('login.html')
+                
+        except Exception as e:
+            return str(e)
+        finally:
+            cursor.close()
+
+
+# 獲取註冊視窗的資料
+@app.route('/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+
+        # 獲取前端輸入的數據
+        id = request.form['regis_id']
+        password = request.form['regis_password']
+        name = request.form['name']
+
+        # 插入數據到 MySQL
+        try:
+            conn = get_db_conn()
+            with conn.cursor() as cursor:
+                sql = 'INSERT INTO user VALUES (%s, %s, %s)'
+                cursor.execute(sql, (id, password, name))
+                conn.commit()
+            return render_template('register.html')
+        except Exception as e:
+            return str(e)
+        finally:
+            cursor.close()
+
+if __name__ == '__main__':
+    app.run(debug=True)
