e2e7853a18387251399387d9216ee6a0197567ab
Poyi1028 <111306057@g.nccu.edu.tw>
2024-05-21 14:15:28 +0800

    Integrate Homepage Function

--- app.py
+++ app.py
@@ -82,10 +82,47 @@ def diet_record():
 def workout_record():
     return render_template('workout_record.html')
 
-# 菜單頁面
+# 菜單主頁面
 @app.route('/menu')
 def menu():
     return render_template('plan01.html')
+
+# 健身菜單頁面
+@app.route('/menu.workout')
+def workout_menu():
+    return render_template('plan02-exercise.html')
+
+# 飲食菜單頁面
+@app.route('/menu.nutrition')
+def nutrition_menu():
+    return render_template('plan02-food.html')
+
+# 搜尋主頁面
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+# 健身房搜尋頁面
+@app.route('/gym')
+def gym():
+    raw = open("templates/gym.html", "r", encoding="utf-8").read()
+    information = '<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d115716.86798982904!2d121.51359254367352!3d24.99494630977675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1z5YGl6Lqr5oi_!5e0!3m2!1sen!2stw!4v1716196040209!5m2!1sen!2stw" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+    return raw.replace("<?tag?>", information)
+
+# 器材搜尋頁面
+@app.route('/equipment')
+def equipment():
+    return  render_template('equipment.html')
+
+# 教練搜尋頁面
+@app.route('/coach')
+def coach():
+    return render_template('coach.html')
+
+# 課程搜尋頁面
+@app.route('/course')
+def course():
+    return render_template('course.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
