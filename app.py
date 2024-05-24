1af609ab57de29e471da4142486cfa4426a6133d
Poyi1028 <111306057@g.nccu.edu.tw>
2024-05-24 20:43:39 +0800

    Gym Search Edit

--- app.py
+++ app.py
@@ -103,8 +103,8 @@ def diet_record_search():
                 cal, pro, car, fat, water = result
             else:
                 return "No data found!"
-
-        return render_template('diet_record.html', date = date, calary = cal, protein = pro, carbon = car, fath = fat, waterh = water)
+        return render_template('diet_record.html', date = date, calary = cal, protein = pro,
+                               carbon = car, fath = fat, waterh = water)
     except Exception as e:
         return str(e)
     finally:
@@ -138,9 +138,7 @@ def search():
 # 健身房搜尋頁面
 @app.route('/gym')
 def gym():
-    raw = open("templates/gym.html", "r", encoding="utf-8").read()
-    information = '<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d115716.86798982904!2d121.51359254367352!3d24.99494630977675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1z5YGl6Lqr5oi_!5e0!3m2!1sen!2stw!4v1716196040209!5m2!1sen!2stw" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
-    return raw.replace("<?tag?>", information)
+    return render_template('gym.html')
 
 # 器材搜尋頁面
 @app.route('/equipment')
