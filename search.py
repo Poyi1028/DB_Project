6391427f4e995f3b593579e81fc442d66644e87d
StanleyWang03 <111306085@nccu.edu.tw>
2024-05-29 14:39:02 +0800

    0529sw

--- search.py
+++ search.py
@@ -1,17 +1,18 @@
 import pymysql
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 # MySQL 連接
-def get_data(table):
+def get_data(table, restriction=""):
     # Establish connection
-    conn = pymysql.connect(host='localhost', user='root', password='sw0217', database='mydb')
+    conn = pymysql.connect(host='localhost', user='root', password='sw0217', database='mydb', charset='utf8mb4')
     # Create a cursor
     cursor = conn.cursor(pymysql.cursors.DictCursor)  # Using DictCursor to return data as dictionaries
     try:
         # SQL query
-        query = "SELECT * FROM " + table  # Replace 'your_table_name' with the actual table name
+        query = f"SELECT * FROM {table} {restriction}"  # Replace 'your_table_name' with the actual table name
+        
         # Execute the query
         cursor.execute(query)
         # Fetch all rows
@@ -29,17 +30,17 @@ def get_data(table):
 
 @app.route('/')
 def index():
-    return open("search.html", "r", encoding="utf-8").read()
+    return render_template('search.html')
 
 @app.route('/gym')
 def gym():
-    raw = open("gym.html", "r", encoding="utf-8").read()
+    raw = open("templates\gym.html", "r", encoding="utf-8").read()
     information = '<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d115716.86798982904!2d121.51359254367352!3d24.99494630977675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1z5YGl6Lqr5oi_!5e0!3m2!1sen!2stw!4v1716196040209!5m2!1sen!2stw" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
     return raw.replace("<?tag?>", information)
 
 @app.route('/equipment')
 def equipment():
-    raw = open("equipment.html", "r", encoding="utf-8").read()
+    raw = open("templates\equipment.html", "r", encoding="utf-8").read()
     gymOptions = "\n".join(['<option value="' + gym['Name'] + '">' + gym['Name'] + '</option>' for gym in get_data("gym")])
     print(get_data("equipment_training_area"))
     equipmentOptions = "\n".join(['<option value="' + gym['Training_Area'] + '">' + gym['Training_Area'] + '</option>' for gym in get_data("equipment_training_area")])
@@ -47,19 +48,26 @@ def equipment():
 
 @app.route('/coach')
 def coach():
-    raw = open("coach.html", "r", encoding="utf-8").read()
+    raw = open("templates\coach.html", "r", encoding="utf-8").read()
     gymOptions = "\n".join(['<option value="' + gym['Name'] + '">' + gym['Name'] + '</option>' for gym in get_data("gym")])
     expertiseOptions = "\n".join(['<option value="' + gym['Expertise'] + '">' + gym['Expertise'] + '</option>' for gym in get_data("coach")])
+    print(request.args.get("branch"), request.args.get("expertise"))
+    
+    restriction = f"Where (Gym_ID = '{request.args.get("branch")}' or Gym_ID IS NULL) and Expertise = '{request.args.get("expertise")}'"
+    
+    print(get_data("coach", restriction) )
     return raw.replace("<?branchOptions?>", gymOptions).replace("<?expertiseOptions?>", expertiseOptions)
-    return open("coach.html", "r", encoding="utf-8").read()
+    return open("templates\coach.html", "r", encoding="utf-8").read()
 
 @app.route('/course')
 def course():
-    raw = open("course.html", "r", encoding="utf-8").read()
+    raw = open("templates\course.html", "r", encoding="utf-8").read()
     gymOptions = "\n".join(['<option value="' + gym['Name'] + '">' + gym['Name'] + '</option>' for gym in get_data("gym")])
     courseOptions = "\n".join(['<option value="' + gym['Name'] + '">' + gym['Name'] + '</option>' for gym in get_data("course")])
+    # get_data("caoch", "Gym_ID = +")
+    # print(request.args.get("branch"], request.args.get("expertise"])
     return raw.replace("<?branchOptions?>", gymOptions).replace("<?courseOptions?>", courseOptions)
-    return open("course.html", "r", encoding="utf-8").read()
+    return open("templates\course.html", "r", encoding="utf-8").read()
 
 # Run the application if this script is executed
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -4,23 +4,21 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # Route to fetch data
-@app.route('/getdata')
-def get_data(table):
+def get_data(table, restriction=""):
     # Establish connection
-    conn = pymysql.connect(host='localhost', user='root', password='sw0217', database='mydb')
+    conn = pymysql.connect(host='localhost', user='root', password='sw0217', database='mydb', charset='utf8mb4')
     # Create a cursor
     cursor = conn.cursor(pymysql.cursors.DictCursor)  # Using DictCursor to return data as dictionaries
     try:
         # SQL query
-        query = "SELECT * FROM " + table  # Replace 'your_table_name' with the actual table name
+        query = "SELECT * FROM " + table + restriction  # Replace 'your_table_name' with the actual table name
         # Execute the query
         cursor.execute(query)
         # Fetch all rows
         rows = cursor.fetchall()
         # Convert query results to a list of dictionaries
         results = [dict(row) for row in rows]
-
-        return jsonify(results)  # Return results as JSON
+        return results  # Return results as JSON
     except Exception as e:
         return str(e)  # Return error message if something goes wrong
     finally:
@@ -28,94 +26,4 @@ def get_data(table):
         cursor.close()
         conn.close()
 
-
-def add_equipment():
-    # Retrieve data from form
-    Gym_ID = 6000
-    Equipment_ID = 6000
-    Address = "1 place"
-    Name = "1 gym"
-
-    # Establish connection
-    conn = pymysql.connect(host='localhost', user='root', password='sw0217', database='mydb')
-    cursor = conn.cursor()
-    
-    try:
-        # Insert data into the gym table
-        cursor.execute("INSERT INTO gym (Gym_ID, Equipment_ID, Address, Name) VALUES (%s, %s, %s, %s)", (Gym_ID, Equipment_ID, Address, Name))
-        conn.commit()  # Commit to save changes
-        return "somewhat worked"
-    except Exception as e:
-        conn.rollback()  # Rollback in case of error
-        return str(e)  # Return the error message
-    finally:
-        cursor.close()
-        conn.close()
-
-def add_equipment():
-    # Retrieve data from form
-    Equipment_ID = 6000
-    Training_Area = "back"
-
-    # Establish connection
-    conn = pymysql.connect(host='localhost', user='root', password='sw0217', database='mydb')
-    cursor = conn.cursor()
-    
-    try:
-        # Insert data into the gym table
-        cursor.execute("INSERT INTO equipment_training_area (Equipment_ID, Training_Area) VALUES (%s, %s)", (Equipment_ID, Training_Area))
-        conn.commit()  # Commit to save changes
-        return "somewhat worked"
-    except Exception as e:
-        conn.rollback()  # Rollback in case of error
-        return str(e)  # Return the error message
-    finally:
-        cursor.close()
-        conn.close()
-
-def add_coach():
-    # Retrieve data from form
-    Coach_ID = 6000
-    Name = "coachA"
-    Expertise = "胸"
-
-    # Establish connection
-    conn = pymysql.connect(host='localhost', user='root', password='sw0217', database='mydb')
-    cursor = conn.cursor()
-    
-    try:
-        # Insert data into the gym table
-        cursor.execute("INSERT INTO coach (Coach_ID, Name, Expertise) VALUES (%s, %s, %s)", (Coach_ID, Name, Expertise))
-        conn.commit()  # Commit to save changes
-        return "somewhat worked"
-    except Exception as e:
-        conn.rollback()  # Rollback in case of error
-        return str(e)  # Return the error message
-    finally:
-        cursor.close()
-        conn.close()
-
-def add_course():
-    # Retrieve data from form
-    Course_ID = 6000
-    Name = "十人十一腳"
-
-    # Establish connection
-    conn = pymysql.connect(host='localhost', user='root', password='sw0217', database='mydb')
-    cursor = conn.cursor()
-    
-    try:
-        # Insert data into the gym table
-        cursor.execute("INSERT INTO course (Course_ID, Name) VALUES (%s, %s)", (Course_ID, Name))
-        conn.commit()  # Commit to save changes
-        return "somewhat worked"
-    except Exception as e:
-        conn.rollback()  # Rollback in case of error
-        return str(e)  # Return the error message
-    finally:
-        cursor.close()
-        conn.close()
-
-# Run the application if this script is executed
-if __name__ == '__main__':
-    print(add_course())
+print(get_data("coach", " WHERE Expertise = '腹肌'"))
